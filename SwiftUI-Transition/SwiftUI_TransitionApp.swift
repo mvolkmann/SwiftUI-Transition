bda4ec36eddfe4579ff29d5e988df42a6abf4a17
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-29 10:40:03 -0500

    Initial Commit

---
+++ SwiftUI-Transition/SwiftUI_TransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TransitionApp.swift
+//  SwiftUI-Transition
+//
+//  Created by R. Mark Volkmann on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
